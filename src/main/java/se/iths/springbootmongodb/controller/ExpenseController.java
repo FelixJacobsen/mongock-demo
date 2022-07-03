f169ccd49822f1e0190b974d8b1d46ee6a7028bd
FelixJacobsen <felix.jacobsen@iths.se>
2022-07-03 11:34:58 +0200

    Add ExpenseController

---
+++ src/main/java/se/iths/springbootmongodb/controller/ExpenseController.java
@@ -0,0 +1,29 @@
+package se.iths.springbootmongodb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expense")
+public class ExpenseController {
+
+    public void addExpense() {
+        //TODO Implement add expense method
+    }
+
+    public void updateExpense() {
+        //TODO Implement update expense method
+    }
+
+    public void getAllExpenses() {
+        //TODO Implement get all expenses method
+    }
+
+    public void getExpenseByName() {
+        //TODO Implement a method to get all expenses by name
+    }
+
+    public void deleteExpense() {
+        //TODO Implement method to delete a expense
+    }
+}
