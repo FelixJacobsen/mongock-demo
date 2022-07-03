62496cd393817c021818c972e27a6f23ce26f105
FelixJacobsen <felix.jacobsen@iths.se>
2022-07-03 11:35:55 +0200

    Add ExpenseService

---
+++ src/main/java/se/iths/springbootmongodb/service/ExpenseService.java
@@ -0,0 +1,27 @@
+package se.iths.springbootmongodb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpenseService {
+
+    public void addExpense() {
+        //TODO Implement add expense method
+    }
+
+    public void updateExpense() {
+        //TODO Implement update expense method
+    }
+
+    public void getAllExpenses() {
+        //TODO Implement get all expenses method
+    }
+
+    public void getExpenseByName() {
+        //TODO Implement a method to get all expenses by name
+    }
+
+    public void deleteExpense() {
+        //TODO Implement method to delete a expense
+    }
+}
